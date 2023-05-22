487c26e0fd4de57ec5bf518dce4c5cd22aa811b5
kodeson <deffuls@debian-BULLSEYE-live-builder-AMD64>
2023-05-22 16:24:41 +0300

    fixed TREE::SEARCH. Now match attributes works

--- DeffulsHTMLParser/search/searching.h
+++ DeffulsHTMLParser/search/searching.h
@@ -3,6 +3,7 @@
 #include "../tags/html_tags.h"
 #include "../signals/signals.h"
 #include <string>
+#include <iostream>
 #include <map>
 
 namespace Options{
@@ -10,9 +11,10 @@ namespace Options{
     struct SearchOption{
         public:
         SearchOption(std::string tagName) : mTagName(tagName) {}
+        SearchOption(std::string tagName, mapAttributes attributes) : mTagName(tagName), mAttributes(attributes) {}
         SearchOption(std::string tagName, mapAttributes attributes, bool matchAttributesCount, int searchDeep = 0)
         : mTagName(tagName), mSearchDeep(searchDeep), mAttributes(attributes) {}
-        
+
         private:
         mapAttributes mAttributes;
         std::string mTagName;
@@ -47,22 +49,24 @@ namespace Search{
             std::size_t cOptMatches = 0;
             std::size_t cOptNeeded = OptAttr.size();
 
-            mapAttributes::const_iterator tagBegin = TagAttr.begin();
             mapAttributes::const_iterator optBegin = OptAttr.begin();
+            mapAttributes::const_iterator tagBegin = TagAttr.begin();
 
-             for(optBegin; optBegin != OptAttr.end(); optBegin++){ // "key" : "value", "key1": "value1"
-                if (optBegin->first.size() > 0 ){
+             for(optBegin; optBegin != OptAttr.end(); optBegin++){
 
+                if ( TagAttr.find(optBegin->first) == TagAttr.end() ) { continue; }
+
+                for ( tagBegin; tagBegin != TagAttr.end(); tagBegin++ ){
+                    if (tagBegin->second.find(optBegin->second) != std::string::npos){
+                        cOptMatches++;
+                        break;
+                    }
                 }
-
-                if(optBegin->second.size() > 0){
-
-                }
-
              }
-             ( cOptMatches == cOptNeeded ) ? cMatch++ : 0 ;
-        }
 
+             ( cOptMatches == cOptNeeded ) ? cMatch++ : cMatch = 0 ;
+        }
+        std::cout << "MATCH : " << cMatch << std::endl;
         return (cMatch > 0) ? true : false;
     }
 
